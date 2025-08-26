644a16a6835b6de97b1adf1049f3ff505223a95c
Lionel Rozario <lionelrozario98@gmail.com>
2025-08-26 16:58:25 -0400

    fixed the code in web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         arxiv_id = request.form["arxiv_id"]
+        
         return f"Recieved arxiv ID: {arxiv_id}"
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
